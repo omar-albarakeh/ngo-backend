bc410c23bd7876f84bcc4feb1744e76bad15842a
omar albarakeh <omaralbarakeh2@gmail.com>
2025-05-02 18:39:39 +0300

    port changed to 10000

--- server.js
+++ server.js
@@ -269,7 +269,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
